bb40d134d8d1f2521ff95d241e0874af96dedb41
Haoran_Yin <j.yinhaoran@gmail.com>
2018-09-23 15:56:24 -0400

    Start

---
+++ HW4/src/DNACompressor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * This is CS180 HW4
+ * The program is used to rearrange DNA sequence
+ * @author Haoran Yin
+ * @version 9/23/2018-0
+ */
+public class DNACompressor {
+    System.out.println("");
+
+}
